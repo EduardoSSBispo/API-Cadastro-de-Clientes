4cedc54d36ac704727b6c91a4d4897722846122f
EduardoSSB <eduardossbispo31@gmail.com>
2024-10-15 18:47:10 -0300

    Envio de documentação relacionada ao projeto

--- Banco de Dados/Scripts de Criação do banco.sql
+++ Banco de Dados/Scripts de Criação do banco.sql
@@ -5,32 +5,23 @@ USE API_CADASTRO_CLIENTE
 CREATE TABLE Cliente(
 	Id int primary key,
 	Nome varchar(100),
-	Email varchar(100) unique,
+	Email varchar(100) unique not null,
 	Logotipo varbinary(Max),
 )
 
 CREATE TABLE Logradouro(
 	Id int identity(1,1) primary key,
 	Nome varchar(100),
-	ClienteId int FOREIGN KEY (clienteId) REFERENCES Cliente(Id) ON DELETE CASCADE
+	ClienteId int not null 
+	FOREIGN KEY (clienteId) REFERENCES Cliente(Id) ON DELETE CASCADE
 )
+-- Índice no campo ClienteId para otimizar JOINs e filtros
+CREATE INDEX IX_Logradouro_ClienteId ON Logradouro (ClienteId);
 
 CREATE TABLE Usuario(
 	Id int identity(1,1) primary key,
 	Nome varchar(100),
-	Email varchar(100) unique,
-	HashSenha varbinary(Max),
-	SaltSenha varbinary(Max)
+	Email varchar(100) unique not null,
+	HashSenha varbinary(Max) not null,
+	SaltSenha varbinary(Max) not null
 )
-
-drop table Logradouro
-drop table Cliente
-drop table Usuario
-
-delete from Cliente
-delete from Logradouro
-delete from Usuario
-
-select * from Cliente
-select * from Logradouro
-select * from Usuario
